b5f2a95eb1eca540a1c2408874bbcd0ba11968ce
yazzy4 <yazzy4@gmail.com>
2019-01-28 13:01:34 -0500

    Duck is weird, but cat and dog are working

--- AnimationsCheckIn/ViewController.swift
+++ AnimationsCheckIn/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var duck: UIImageView!
+    @IBOutlet weak var dogOrCat: UILabel!
+    @IBOutlet weak var dog: UIButton!
+    
+    private var animator: UIViewPropertyAnimator!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeInOut, animations: {
+            self.duck.transform = CGAffineTransform(translationX: 100.0, y: 100.0)
+        })
     }
 
+    @IBAction func slider(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
+    }
+    
+    @IBAction func transformButtonPressed(_ sender: Any) {
+        if dog.imageView?.image == UIImage(named: "dog") {
+            UIView.transition(with: dog, duration: 1.0, options: [.transitionFlipFromRight], animations: {
+                self.dog.setImage(UIImage(named: "cat"), for: .normal)
+                self.dogOrCat.text = "cat"
+            })
+        } else{
+            UIView.transition(with: dog, duration: 1.0, options: [.transitionFlipFromLeft], animations: {
+                self.dog.setImage(UIImage(named: "dog"), for: .normal)
+                self.dogOrCat.text = "dog"
+            })
+        }
+        
+    }
+    
 
 }
-
